d30689edab48f61e666e2069961d35e7d3207042
Swoad-11 <toufiq11swoad@gmail.com>
2022-05-10 16:38:14 +0600

    server side connected

--- src/Components/AddItems/AddItems.js
+++ src/Components/AddItems/AddItems.js
@@ -9,7 +9,7 @@ const AddItems = () => {
 
     const onSubmit = data => {
         console.log(data);
-        const url = 'http://localhost:5000/cars';
+        const url = 'http://localhost:5000/items';
         fetch(url, {
             method: 'POST',
             headers: {
@@ -30,13 +30,43 @@ const AddItems = () => {
             <div>
                 <h1 className="secondary-title text-4xl font-bold mt-8 mb-5 text-center text-red-600">Add Items</h1>
             </div>
-            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
-                <textarea className='mb-2' placeholder='Description' {...register("description")} />
-                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
-                <input className='mb-2' placeholder='Photo URL' type="text" {...register("image")} />
-                <input type="submit" value="Add Car" />
-            </form>
+            <div className="description block p-6 rounded-lg shadow-lg bg-white max-w-sm my-8 mx-auto border-2">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputEmail2" className="form-label inline-block mb-2 text-gray-700">Name</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+                            aria-describedby="emailHelp" placeholder="Name" {...register("name", { required: true, maxLength: 30 })} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Description</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+                            placeholder="Description" {...register("description")} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Image URL</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+                            placeholder="Image URL" {...register("image")} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Quantity</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+                            placeholder="Quantity" {...register("quantity")} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Supplier Name</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+                            placeholder="Supplier Name" {...register("supplier")} />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Price</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none"
+                            placeholder="Price" {...register("price")} />
+                    </div>
+
+                    <button type="submit" className=" w-full px-6 py-2.5 bg-red-600 text-white font- text-lg leading-tight rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Add Item</button>
+                </form>
+            </div>
+
         </div>
     );
 };
