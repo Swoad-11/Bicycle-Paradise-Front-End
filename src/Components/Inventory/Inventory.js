07677f61ec6ce72546470ccf4b860681f7104c66
Swoad-11 <toufiq11swoad@gmail.com>
2022-05-10 11:34:59 +0600

    delivered,restock button wokring

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -7,6 +7,35 @@ const Inventory = () => {
     console.log(itemId);
     const [item] = useItemDetail(itemId);
     const { _id, name, image, description, price, quantity, supplier } = item;
+
+    const decreaseItem = () => {
+        let quantity = document.getElementById('quantity');
+        let quantityNumber = quantity.innerText;
+
+        let newQuantity = parseInt(quantityNumber) - 1;
+        if (newQuantity > 0) {
+            quantity.innerText = newQuantity;
+        }
+        else {
+            const zero = 0;
+            quantity.innerText = zero;
+        }
+    }
+
+    const restockItem = (e) => {
+        e.preventDefault();
+        const inputQuantity = document.getElementById('inputQuantity');
+        const newQuantity = inputQuantity.value;
+
+        let quantity = document.getElementById('quantity');
+        let quantityNumber = quantity.innerText;
+        if (newQuantity > 0) {
+            let totalQuantity = parseInt(quantityNumber) + parseInt(newQuantity);
+            quantity.innerText = totalQuantity;
+        }
+    }
+
+
     return (
         <div>
             <div>
@@ -29,7 +58,7 @@ const Inventory = () => {
                             <span className='font-semibold'>Description: </span> {description}
                         </p>
                         <p className='description text-gray-700 text-base my-5'>
-                            <span className='font-semibold'>Quanity: </span> {quantity}
+                            <span className='font-semibold'>Quanity: </span> <span id='quantity'>{quantity}</span>
                         </p>
                         <p className='description text-gray-700 text-base my-5'>
                             <span className='font-semibold'>Supplier: </span> {supplier}
@@ -37,17 +66,17 @@ const Inventory = () => {
                         <p className='description text-base my-5 text-red-500'>
                             <span className='font-semibold text-gray-700'>Price: </span> {price}
                         </p>
-                        <button type="button" className="secondary-title inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Delivered</button>
+                        <button onClick={decreaseItem} type="button" className="secondary-title inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Delivered</button>
                     </div>
 
-                    <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm mx-auto">
-                        <form>
+                    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm mx-auto">
+                        <form onSubmit={restockItem}>
                             <h1 className="secondary-title text-xl font-bold mt-8 mb-2 text-center text-red-600"><span className='text-gray-700'>Restock Product</span> {_id}</h1>
-                            <div class="form-group mb-6">
-                                <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput90"
-                                    placeholder="Quantity" />
+                            <div className="form-group mb-6">
+                                <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="inputQuantity"
+                                    placeholder="0" />
                             </div>
-                            <button type="submit" class=" w-full px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg  focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Restock</button>
+                            <button type="submit" className=" w-full px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg  focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Restock</button>
                         </form>
                     </div>
 
