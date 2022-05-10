6c922aacd08c79c7f48d97dc67354aa58902bd69
Swoad-11 <toufiq11swoad@gmail.com>
2022-05-10 15:49:26 +0600

    add item page

--- src/Components/AddItems/AddItems.js
+++ src/Components/AddItems/AddItems.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddItems = () => {
+
+    const [item, setItem] = useState([])
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'http://localhost:5000/cars';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+
+                const newItem = [...item, data];
+                setItem(newItem);
+            })
+    };
+
     return (
         <div>
             <div>
                 <h1 className="secondary-title text-4xl font-bold mt-8 mb-5 text-center text-red-600">Add Items</h1>
             </div>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                <textarea className='mb-2' placeholder='Description' {...register("description")} />
+                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+                <input className='mb-2' placeholder='Photo URL' type="text" {...register("image")} />
+                <input type="submit" value="Add Car" />
+            </form>
         </div>
     );
 };
--- src/Components/ManageInventory/ManageInventory.js
+++ src/Components/ManageInventory/ManageInventory.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useItems from '../../Hooks/useItems';
 import Items from '../../Items/Items';
 
@@ -18,6 +19,11 @@ const ManageInventory = () => {
                         >
                         </Items>)
                     }
+                </div>
+                <div className='flex space-x-2 justify-center mb-8'>
+                    <Link to="/additems">
+                        <button type="button" className="secondary-title inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Add New Item</button>
+                    </Link>
                 </div>
             </div>
         </div>
